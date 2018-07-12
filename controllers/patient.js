aa15484c876c9d1faa11b5604fa0471681a08582
nmzz <071bct541@pcampus.edu.np>
2018-07-12 14:44:04 +0545

    dashboard display

--- controllers/patient.js
+++ controllers/patient.js
@@ -1,33 +1,6 @@
 var Models = require('../models');
 module.exports = {
     index: function (req, res) {
-<<<<<<< HEAD
-    //     var viewModel = {
-    //         patient: {}
-    //     };
-    //     Models.Patient.findOne({
-    //         'ethAddr': req.params.firstAccount
-    //     }, function (err, patient) {
-    //         if (err) {
-    //             throw err;
-    //         }
-    //         if (!err && patient) {
-    //             viewModel.patient = patient;
-
-    //             console.log(viewModel.patient.personalDetail.profilePic);
-    //             res.render(viewModel, {
-    //                 title: 'patientdashboard',
-    //                 layout: 'base'
-    //             });
-    //         }
-    //     });
-    // }
-    
-    res.render('patientdashboard', {
-                        title: 'patientdashboard',
-                        layout: 'base'
-                    });
-=======
         var viewModel = {
             patient: {}
         };
@@ -44,6 +17,5 @@ module.exports = {
                 res.render( 'patientdashboard',viewModel);
             }
         });
->>>>>>> 7f97328f519521f134047d4589b05e97f3dd6231
     }
 }
