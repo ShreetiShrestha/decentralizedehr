aa96119f477e427436b509780f2d0d2ffd45f8d5
nmzz <071bct541@pcampus.edu.np>
2018-07-12 14:00:42 +0545

    issue of patient dashboard default layout resolved

--- controllers/patient.js
+++ controllers/patient.js
@@ -1,29 +1,21 @@
 var Models = require('../models');
 module.exports = {
     index: function (req, res) {
-    //     var viewModel = {
-    //         patient: {}
-    //     };
-    //     Models.Patient.findOne({
-    //         'ethAddr': req.params.firstAccount
-    //     }, function (err, patient) {
-    //         if (err) {
-    //             throw err;
-    //         }
-    //         if (!err && patient) {
-    //             viewModel.patient = patient;
-
-    //             console.log(viewModel.patient.personalDetail.profilePic);
-    //             res.render(viewModel, {
-    //                 title: 'patientdashboard',
-    //                 layout: 'base'
-    //             });
-    //         }
-    //     });
-    // }
-    res.render('patientdashboard', {
-                        title: 'patientdashboard',
-                        layout: 'base'
-                    });
+        var viewModel = {
+            patient: {}
+        };
+        Models.Patient.findOne({
+            'ethAddr': req.params.firstAccount
+        }, function (err, patient) {
+            if (err) {
+                throw err;
+            }
+            if (!err && patient) {
+                
+                viewModel.patient = patient;
+                console.log(viewModel.patient.personalDetail.profilePic);
+                res.render( 'patientdashboard',viewModel);
+            }
+        });
     }
 }
