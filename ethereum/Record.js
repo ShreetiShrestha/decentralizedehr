1f7044b67cb537c4f18d07471f6974714b716630
nmzz <071bct541@pcampus.edu.np>
2018-07-28 14:23:53 +0545

    trying to implement blockchain part

---
+++ ethereum/Record.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+import Record from './build/Record.json';
+const instance = new web3.eth.Contract(
+    JSON.parse(Record.interface),
+    '0xd535d7D49eAC07e83b360E5D67EB1353E34b9720'
+);
+export default instance; 
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,14 +1,11 @@
-// var Web3 = require('web3');
-
-
-// let web3;
-// // var window= Web3.window;
-// if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-//     console.log('you have metamask');
-//     web3 = new Web3(window.web3.currentProvider);
-// } else {
-//     web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/F7F0FVQU2NvNwqRLrwHn'));
-//     console.log('No web3? You should consider trying MetaMask!');
-// }
-
-// exports.web3 = web3;
+var Web3 = require('web3');
+var web3;
+// var window= Web3.window;
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    console.log('you have metamask');
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/F7F0FVQU2NvNwqRLrwHn'));
+    console.log('No web3? You should consider trying MetaMask!');
+}
+exports.web3 = web3; 
